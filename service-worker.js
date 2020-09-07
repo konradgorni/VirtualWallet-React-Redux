edd7b96f15a0c8c3b252bbdbf59f7f6d7e7d7a9d
Konrad Górni <gornikonrad@gmail.com>
2020-09-07 23:53:18 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/konradgorni/VirtualWallet-React-Redux/precache-manifest.7bb5c13e20bfa2dabf0e68bb057dce02.js"
+  "/VirtualWallet-React-Redux/precache-manifest.8283071c626cf1abe92ef7525c3ca4b0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/konradgorni/VirtualWallet-React-Redux/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/VirtualWallet-React-Redux/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
